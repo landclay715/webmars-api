e1a6b52c89433a1ad00ae73c7246cb5a4315aa10
landclay715 <landclay715@gmail.com>
2026-05-17 13:11:10 -0700

    feat: Flyway migrations, FlywayConfig, auth 401 fix, security hardening

---
+++ webmars-api/src/main/java/com/webmars/webmars_api/FlywayConfig.java
@@ -0,0 +1,29 @@
+package com.webmars.webmars_api;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String user;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        return Flyway.configure()
+                .dataSource(url, user, password)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .baselineVersion("1")
+                .load();
+    }
+}
